ee22afbcb79db3573606b3ae99f3ffe404d8e099
TMJohnson1203 <tmjohnson1203@gmail.com>
2017-09-27 19:14:00 -0400

    initial commit

---
+++ Conditionals_HW/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals_HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // are the two intergers equal to each other?
+            // if yes, tell user that they are equal
+            // if else, tell user that they are not equal
+
+            int firstNum = int.Parse(ReadLine());
+            Console.WriteLine("Please enter a number.");
+           
+            int secNum = int.Parse(Readline());
+            Console.WriteLine("Please enter another number");
+
+            if firstNumber == secNum
+
+            
+            Console.WriteLine("")
+
+
+           
+
+
+         
+
+        }
+    }
+}
