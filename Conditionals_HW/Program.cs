924af2a0622f2c2cc9760d65b4e35b19087b0b8b
TMJohnson1203 <tmjohnson1203@gmail.com>
2017-09-28 10:21:58 -0400

    commit

--- Conditionals_HW/Program.cs
+++ Conditionals_HW/Program.cs
@@ -15,16 +15,54 @@ namespace Conditionals_HW
             // if yes, tell user that they are equal
             // if else, tell user that they are not equal
 
-            int firstNum = int.Parse(ReadLine());
-            Console.WriteLine("Please enter a number.");
+            //int firstNum = int.Parse(ReadLine());
+            //Console.WriteLine("Please enter a number.");
            
-            int secNum = int.Parse(Readline());
-            Console.WriteLine("Please enter another number");
+            //int secNum = int.Parse(Readline());
+            //Console.WriteLine("Please enter another number");
 
-            if firstNumber == secNum
+            //if firstNumber == secNum
 
             
-            Console.WriteLine("")
+            //Console.WriteLine("")
+
+                    // problem 4
+                    // is the character entered a vowel or a consonant
+
+                //Console.WriteLine("Please enter a letter")
+                //    char yourLetter = char.Parse(Console.ReadLine());
+
+                //switch yourLetter == 'a'
+                //    {
+                //    Console.WriteLine("Your letter is a vowel.");
+                //    }
+
+                // number 5
+
+            Console.WriteLine("Enter a whole number");
+            int firstValue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter another whole number");
+            int secondValue = int.Parse(Console.ReadLine());
+
+            if (firstValue > secondValue)
+            {
+                Console.WriteLine(firstValue + "is the larger value.");
+            }
+
+            else if (secondValue > firstValue)
+            {
+                Console.WriteLine(secondValue + "is the larger value.");
+            }
+
+            else
+            {
+                Console.WriteLine("The numbers are equal.");
+            }
+
+
+    
+
 
 
            
